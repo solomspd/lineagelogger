bd6ba918e284c9fd966db9c00f811dd2d537f7c8
Abdelsalam ElTamawy <abdelsalam@solom.dev>
2024-03-01 13:28:48 +0200

    starter finally complete

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,60 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { writable } from "svelte/store";
+  import {
+    SvelteFlow,
+    Controls,
+    Background,
+    BackgroundVariant,
+    MiniMap,
+  } from "@xyflow/svelte";
+
+  // 👇 this is important! You need to import the styles for Svelte Flow to work
+  import "@xyflow/svelte/dist/style.css";
+
+  // We are using writables for the nodes and edges to sync them easily. When a user drags a node for example, Svelte Flow updates its position.
+  const nodes = writable([
+    {
+      id: "1",
+      type: "input",
+      data: { label: "Input Node" },
+      position: { x: 0, y: 0 },
+    },
+    {
+      id: "2",
+      type: "default",
+      data: { label: "Node" },
+      position: { x: 0, y: 150 },
+    },
+  ]);
+
+  // same for edges
+  const edges = writable([
+    {
+      id: "1-2",
+      type: "default",
+      source: "1",
+      target: "2",
+      label: "Edge Text",
+    },
+  ]);
+
+  const snapGrid = [25, 25];
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<!--
+👇 By default, the Svelte Flow container has a height of 100%.
+This means that the parent container needs a height to render the flow.
+-->
+<div style:height="100vh">
+  <SvelteFlow
+    {nodes}
+    {edges}
+    {snapGrid}
+    fitView
+    on:nodeclick={(event) => console.log("on node click", event.detail.node)}
+  >
+    <Controls />
+    <Background variant={BackgroundVariant.Dots} />
+    <MiniMap />
+  </SvelteFlow>
+</div>
