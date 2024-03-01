b85ad8705141666ddd095ea6afbf2abf44f99256
Abdelsalam ElTamawy <abdelsalam@solom.dev>
2024-03-01 14:03:02 +0200

    sidebar in progress

---
+++ src/Canvas.svelte
@@ -0,0 +1,107 @@
+<script lang="ts">
+  import { writable } from "svelte/store";
+  import {
+    SvelteFlow,
+    Controls,
+    Background,
+    BackgroundVariant,
+    MiniMap,
+    useSvelteFlow,
+  } from "@xyflow/svelte";
+  import MemberNode from "./lib/MemberNode.svelte";
+
+  // 👇 this is important! You need to import the styles for Svelte Flow to work
+  import "@xyflow/svelte/dist/style.css";
+
+  const nodeTypes = {
+    member: MemberNode,
+  };
+
+  // We are using writables for the nodes and edges to sync them easily. When a user drags a node for example, Svelte Flow updates its position.
+  const nodes = writable([
+    {
+      id: "1",
+      type: "member",
+      data: { label: "Input Node", color: writable("#ff4000") },
+      position: { x: 0, y: 0 },
+    },
+    {
+      id: "2",
+      type: "default",
+      data: { label: "Node" },
+      position: { x: 0, y: 150 },
+    },
+  ]);
+
+  // same for edges
+  const edges = writable([
+    {
+      id: "1-2",
+      type: "default",
+      source: "1",
+      target: "2",
+    },
+  ]);
+
+  const snapGrid = [25, 25];
+
+  const { screenToFlowPosition } = useSvelteFlow();
+  // const onDragOver = (event: DragEvent) => {
+  //   event.preventDefault();
+
+  //   if (event.dataTransfer) {
+  //     event.dataTransfer.dropEffect = "move";
+  //   }
+  // };
+
+  // const onDrop = (event: DragEvent) => {
+  //   event.preventDefault();
+
+  //   if (!event.dataTransfer) {
+  //     return null;
+  //   }
+
+  //   const type = event.dataTransfer.getData("application/svelteflow");
+
+  //   const position = screenToFlowPosition({
+  //     x: event.clientX,
+  //     y: event.clientY,
+  //   });
+
+  //   const newNode = {
+  //     id: `${Math.random()}`,
+  //     type,
+  //     position,
+  //     data: { label: `${type} node` },
+  //     origin: [0.5, 0.0],
+  //   } satisfies Node;
+
+  //   $nodes.push(newNode);
+  //   $nodes = $nodes;
+  // };
+</script>
+
+<!--
+👇 By default, the Svelte Flow container has a height of 100%.
+This means that the parent container needs a height to render the flow.
+-->
+<main>
+  <SvelteFlow
+    {nodes}
+    {edges}
+    {snapGrid}
+    {nodeTypes}
+    fitView
+    on:nodeclick={(event) => console.log("on node click", event.detail.node)}
+  >
+    <Controls />
+    <Background variant={BackgroundVariant.Dots} />
+    <MiniMap />
+  </SvelteFlow>
+</main>
+
+<style>
+  main {
+    height: 100vh;
+  }
+</style>
